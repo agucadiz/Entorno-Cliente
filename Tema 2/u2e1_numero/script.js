4893b8c38dd9d0af1e34b090ce43fec101b2ec79
Agustin Pedrote <agu.cadiz@hotmail.com>
2021-10-13 21:23:18 +0200

    Miercoles 13 oct

---
+++ Tema 2/u2e1_numero/script.js
@@ -0,0 +1,17 @@
+// variable de tipo entero.
+const entero=1357;
+// variable de tipo decimal.
+const decimal=135.7;
+// variable de tipo científico.
+const cientifico=135e7;
+// variable de tipo oscal.
+const octal=01357;
+// variable de tipo hexadecimal.
+const hexadecimal=0x1357;
+
+
+alert("Entero " + entero);
+alert("Decimal " + entero);
+alert("Científico " + entero);
+alert("Octal " + entero);
+alert("Hexadecimal " + entero);
---
+++ Tema 2/u2e2_cadenas/script.js
@@ -0,0 +1,10 @@
+const nombre = "Agustín"
+const apellido = "Pedrote Bejarano"
+const edad = 38
+const año = 1982
+
+alert("Datos personales");
+alert(nombre + "\n" + edad);
+alert(edad + año);
+alert(nombre + "\n" + apellido + "\n" + edad + "\n" + año);
+
